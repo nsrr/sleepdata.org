9c33a8b664a8c4c47ea45eeeaeb573bd1612a8d1
Remo Mueller <remosm@gmail.com>
2013-11-07 15:59:38 -0500

    Updated welcome controller tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -8,4 +8,46 @@ class WelcomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get wget" do
+    get :wget
+    assert_template 'tools/wget'
+    assert_response :success
+  end
+
+  test "should get wget and ignore user agent" do
+    get :wget, choose: '1'
+    assert_template 'tools/wget'
+    assert_response :success
+  end
+
+  test "should get wget for linux" do
+    @request.user_agent = "Linux"
+    get :wget
+    assert_redirected_to wget_src_path
+  end
+
+  test "should get wget for mac" do
+    @request.user_agent = "Mac OS X"
+    get :wget
+    assert_redirected_to wget_src_path
+  end
+
+  test "should get wget for windows" do
+    @request.user_agent = "Windows"
+    get :wget
+    assert_redirected_to wget_windows_path
+  end
+
+  test "should get wget_src" do
+    get :wget_src
+    assert_template 'tools/wget/wget_src'
+    assert_response :success
+  end
+
+  test "should get wget_windows" do
+    get :wget_windows
+    assert_template 'tools/wget/wget_windows'
+    assert_response :success
+  end
+
 end
