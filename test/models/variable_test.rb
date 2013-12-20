8f30bace71c99c58b148b18f1bb6f5e6665abafd
Remo Mueller <remosm@gmail.com>
2013-12-20 15:41:57 -0500

    Added checks for variable scores

--- test/models/variable_test.rb
+++ test/models/variable_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class VariableTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get variable score" do
+    assert_equal 2, variables(:one).score(['gender'])
+    assert_equal 2.5, variables(:one).score(['gen', 'demographics', 'other'])
+    assert_equal 1.5, variables(:one).score(['gen', 'other'])
+    assert_equal 0.5, variables(:one).score(['other'])
+    assert_equal 0.5, variables(:one).score([])
+
+    assert_equal 1, variables(:two).score(['two', 'other'])
+    assert_equal 1, variables(:two).score(['two'])
+    assert_equal 0, variables(:two).score(['other'])
+    assert_equal 0, variables(:two).score([])
+  end
 end
