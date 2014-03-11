eabe93eb06df0fd628b8d657d56ed2380595939d
Remo Mueller <remosm@gmail.com>
2014-03-11 11:07:38 -0400

    Location should be stored as a before_action not an after_action step

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  after_filter :store_location
+  before_action :store_location
 
   def store_location
     if (params[:action] != 'logo' &&
