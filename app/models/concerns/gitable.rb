08897cda8d517d001b0e3d1520dd4a4abaf73e0d
Remo Mueller <remosm@gmail.com>
2014-03-13 10:34:35 -0400

    Cleaned up debug information

--- app/models/concerns/gitable.rb
+++ app/models/concerns/gitable.rb
@@ -5,24 +5,19 @@ module Gitable
     FileUtils.cd(root_folder)
     stdout = `git rev-parse HEAD`
     commit = (stdout.match(/[0-9a-f]{40}/)[0] rescue '')
-    Rails.logger.info "LOCAL_COMMIT: #{commit}"
     commit
   end
 
   def remote_commit
     FileUtils.cd(root_folder)
-    command = "git ls-remote #{self.git_repository} HEAD"
-    Rails.logger.info command
-    stdout = `#{command}`
+    stdout = `git ls-remote #{self.git_repository} HEAD`
     commit = (stdout.match(/[0-9a-f]{40}/)[0] rescue '')
-    Rails.logger.info "REMOTE_COMMIT: #{commit}"
     commit
   end
 
   def pull_latest!
     FileUtils.cd(root_folder)
     stdout = `git pull`
-    Rails.logger.info "PULL_LATEST!: #{stdout}"
     stdout
   end
 end
