e0469a37550a953fc8fee104440723819c5288e7
Remo Mueller <remosm@gmail.com>
2014-09-23 08:52:32 -0400

    Order agreements by id as well as expiration date

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -147,7 +147,7 @@ class AgreementsController < ApplicationController
   # GET /agreements
   # GET /agreements.json
   def index
-    @agreements = Agreement.current.search(params[:search]).order(expiration_date: :desc).page(params[:page]).per( 40 )
+    @agreements = Agreement.current.search(params[:search]).order([expiration_date: :desc, id: :desc]).page(params[:page]).per( 40 )
   end
 
   # GET /agreements/1
