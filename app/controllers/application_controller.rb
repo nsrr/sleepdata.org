2a534da5806a1c91e76d31dd9abd2a058623e48a
Remo Mueller <remosm@gmail.com>
2014-09-26 16:54:54 -0400

    Sort nulls last when sorting by order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < ActionController::Base
 
   def scrub_order(model, params_order, default_order)
     (params_column, params_direction) = params_order.to_s.strip.downcase.split(' ')
-    direction = (params_direction == 'desc' ? 'DESC' : nil)
+    direction = (params_direction == 'desc' ? 'DESC NULLS LAST' : nil)
     column_name = (model.column_names.collect{|c| model.table_name + "." + c}.select{|c| c == params_column}.first)
     order = column_name.blank? ? default_order : [column_name, direction].compact.join(' ')
     order
