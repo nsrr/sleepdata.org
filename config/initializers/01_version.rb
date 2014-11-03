9be1fcfa520be61bd6065f636480dadf8d293c9c
Remo Mueller <remosm@gmail.com>
2014-11-03 13:56:03 -0500

    Version bump to 0.15.0.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -1,9 +1,9 @@
 module WwwSleepdataOrg
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 14
+    MINOR = 15
     TINY = 0
-    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
