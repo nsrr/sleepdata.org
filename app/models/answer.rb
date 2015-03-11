146f8f553e5449410d6f230aae1419837b352a91
Remo Mueller <remosm@gmail.com>
2015-03-11 11:22:17 -0400

    A user can only have one answer for a given challenge and question combination, addresses #83.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ActiveRecord::Base
 
   # Model Validation
   validates_presence_of :challenge_id, :question_id, :user_id
+  validates_uniqueness_of :user_id, scope: [ :challenge_id, :question_id ]
 
   # Model Relationships
   belongs_to :challenge
