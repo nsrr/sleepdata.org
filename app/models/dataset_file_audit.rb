863cc5891305cd5d21477469d591a8990fbfb803
Remo Mueller <remosm@gmail.com>
2015-08-25 11:20:52 -0400

    Simplified dataset file audit scopes

--- app/models/dataset_file_audit.rb
+++ app/models/dataset_file_audit.rb
@@ -7,7 +7,7 @@ class DatasetFileAudit < ActiveRecord::Base
   # Named Scopes
   scope :audit_before, lambda { |arg| where("dataset_file_audits.created_at < ?", (arg+1.day).at_midnight) }
   scope :audit_after, lambda { |arg| where("dataset_file_audits.created_at >= ?", arg.at_midnight) }
-  scope :regular_members, -> { where("dataset_file_audits.user_id in (select users.id from users where users.deleted = ? and users.aug_member = ? and users.core_member = ?)", false, false, false).references(:users) }
-  scope :aug_or_core_members, -> { where("dataset_file_audits.user_id in (select users.id from users where users.deleted = ? and (users.aug_member = ? or users.core_member = ?))", false, true, true).references(:users) }
+  scope :regular_members, -> { joins(:user).merge(User.current.where(aug_member: false, core_member: false)) }
+  scope :aug_or_core_members, -> { joins(:user).merge(User.current.where("aug_member = ? or core_member = ?", true, true)) }
 
 end
