bd9af1cd2ee02114767eae6ac3748e4597f67ed2
Remo Mueller <remosm@gmail.com>
2015-09-24 16:39:30 -0400

    Simplified test_helper.rb

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-SimpleCov.command_name "test:integration"
+SimpleCov.command_name 'test:integration'
 
 class NavigationTest < ActionDispatch::IntegrationTest
   fixtures :users
@@ -10,31 +10,31 @@ class NavigationTest < ActionDispatch::IntegrationTest
     @deleted = users(:deleted)
   end
 
-  test "should get root path" do
-    get "/"
+  test 'should get root path' do
+    get '/'
     assert_equal '/', path
   end
 
-  test "deleted users should be not be allowed to login" do
+  test 'deleted users should be not be allowed to login' do
     get new_user_session_path
 
-    sign_in_as(@deleted, "123456", "deleted-2@example.com")
+    sign_in_as(@deleted, '123456')
     assert_equal new_user_session_path, path
     assert_equal I18n.t('devise.failure.inactive'), flash[:alert]
   end
 
-  test "friendly url forwarding after login" do
+  test 'friendly url forwarding after login' do
     get datasets_path
     get new_user_session_path
 
-    sign_in_as(@valid, "123456", "valid-2@example.com")
+    sign_in_as(@valid, '123456')
     assert_equal datasets_path, path
     assert_equal I18n.t('devise.sessions.signed_in'), flash[:notice]
   end
 
-  test "friendly url forwarding after logout" do
+  test 'friendly url forwarding after logout' do
     get datasets_path
-    sign_in_as(@valid, "123456", "valid-2@example.com")
+    sign_in_as(@valid, '123456')
 
     get datasets_path
     get destroy_user_session_path
