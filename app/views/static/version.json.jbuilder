56dc3b2ce4a22d67523fc01af97877a1bda528ad
Remo Mueller <remosm@gmail.com>
2015-09-24 16:35:25 -0400

    Added missing attribute to version JSON API

--- app/views/static/version.json.jbuilder
+++ app/views/static/version.json.jbuilder
@@ -1,6 +1,7 @@
 json.version do
   json.string WwwSleepdataOrg::VERSION::STRING
   json.major WwwSleepdataOrg::VERSION::MAJOR
+  json.minor WwwSleepdataOrg::VERSION::MINOR
   json.tiny WwwSleepdataOrg::VERSION::TINY
   json.build WwwSleepdataOrg::VERSION::BUILD
 end
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,40 +1,49 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionController::TestCase
-
   setup do
     @regular_user = users(:valid)
   end
 
-  test "should get demo for logged out user" do
+  test 'should get demo for logged out user' do
     get :demo
     assert_response :success
   end
 
-  test "should get demo for regular user" do
+  test 'should get demo for regular user' do
     login(@regular_user)
     get :demo
     assert_response :success
   end
 
-  test "should get version" do
-    get :version
-    assert_response :success
-  end
-
-  test "should get parallax" do
+  test 'should get parallax' do
     get :parallax
     assert_response :success
   end
 
-  test "should get parallax2" do
+  test 'should get parallax2' do
     get :parallax2
     assert_response :success
   end
 
-  test "should get map" do
+  test 'should get map' do
     get :map
     assert_response :success
   end
 
+  test 'should get version' do
+    get :version
+    assert_response :success
+  end
+
+  test 'should get version as json' do
+    get :version, format: 'json'
+    version = JSON.parse(response.body)
+    assert_equal WwwSleepdataOrg::VERSION::STRING, version['version']['string']
+    assert_equal WwwSleepdataOrg::VERSION::MAJOR, version['version']['major']
+    assert_equal WwwSleepdataOrg::VERSION::MINOR, version['version']['minor']
+    assert_equal WwwSleepdataOrg::VERSION::TINY, version['version']['tiny']
+    assert_equal WwwSleepdataOrg::VERSION::BUILD, version['version']['build']
+    assert_response :success
+  end
 end
