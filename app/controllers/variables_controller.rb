52d84eb941e2b214d116b9ddfcc35a730b3e406f
Remo Mueller <remosm@gmail.com>
2015-12-01 13:07:27 -0500

    Removed commonly used filter, and simplified listing of folders

--- app/controllers/variables_controller.rb
+++ app/controllers/variables_controller.rb
@@ -6,14 +6,13 @@ class VariablesController < ApplicationController
   before_action :set_folder_and_version,    only: [:show, :graphs, :form, :known_issues, :related, :history]
 
   def index
-    variable_scope = @dataset.variables.search(params[:s]).with_folder(params[:folder])
-    variable_scope = variable_scope.where(commonly_used: true) if params[:common] == '1'
+    variable_scope = @dataset.variables.with_folder(params[:folder])
     if params[:s].blank?
       @variables = variable_scope.page(params[:page]).per(100).order('commonly_used desc', :folder, :name)
       @folders = variable_scope.pluck(:folder).uniq.collect { |f| f.gsub(/^#{params[:folder]}(\/)?/, '').split('/').first }.uniq.compact.sort
       @total_count = @variables.total_count
     else
-      @variables = variable_scope
+      @variables = variable_scope.search(params[:s])
       @folders = variable_scope.pluck(:folder).uniq.collect { |f| f.gsub(/^#{params[:folder]}(\/)?/, '').split('/').first }.uniq.compact.sort
       @total_count = @variables.count
     end
