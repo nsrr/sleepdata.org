40cb21b0b3840433a7f66507bd898a708a03596a
Remo Mueller <remosm@gmail.com>
2015-12-02 10:06:54 -0500

    Updated sitemap and external controller

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -1,9 +1,15 @@
 # Displays publicly available pages
 class ExternalController < ApplicationController
+  def about
+  end
+
   def contact
   end
 
   def landing
     @datasets = Dataset.release_scheduled
+  end
+
+  def sitemap
   end
 end
