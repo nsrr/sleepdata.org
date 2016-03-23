a791e6d86989135334dae21add0974507a5dc8f0
Remo Mueller <remosm@gmail.com>
2016-03-23 13:11:09 -0400

    Domains controller is not currently implemented, removing tests

--- test/controllers/api/v1/domains_controller_test.rb
+++ test/controllers/api/v1/domains_controller_test.rb
@@ -3,56 +3,49 @@
 require 'test_helper'
 
 class Api::V1::DomainsControllerTest < ActionController::TestCase
-  setup do
-    @domain = domains(:one)
-  end
+  # setup do
+  #   @domain = domains(:one)
+  # end
 
-  test 'should get index' do
-    skip
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:domains)
-  end
+  # test 'should get index' do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:domains)
+  # end
 
-  test 'should get new' do
-    skip
-    get :new
-    assert_response :success
-  end
+  # test 'should get new' do
+  #   get :new
+  #   assert_response :success
+  # end
 
-  test 'should create domain' do
-    skip
-    assert_difference('Domain.count') do
-      post :create, domain: { create: @domain.create, name: @domain.name }
-    end
+  # test 'should create domain' do
+  #   assert_difference('Domain.count') do
+  #     post :create, domain: { create: @domain.create, name: @domain.name }
+  #   end
 
-    assert_redirected_to domain_path(assigns(:domain))
-  end
+  #   assert_redirected_to domain_path(assigns(:domain))
+  # end
 
-  test 'should show domain' do
-    skip
-    get :show, id: @domain
-    assert_response :success
-  end
+  # test 'should show domain' do
+  #   get :show, id: @domain
+  #   assert_response :success
+  # end
 
-  test 'should get edit' do
-    skip
-    get :edit, id: @domain
-    assert_response :success
-  end
+  # test 'should get edit' do
+  #   get :edit, id: @domain
+  #   assert_response :success
+  # end
 
-  test 'should update domain' do
-    skip
-    patch :update, id: @domain, domain: { create: @domain.create, name: @domain.name }
-    assert_redirected_to domain_path(assigns(:domain))
-  end
+  # test 'should update domain' do
+  #   patch :update, id: @domain, domain: { create: @domain.create, name: @domain.name }
+  #   assert_redirected_to domain_path(assigns(:domain))
+  # end
 
-  test 'should destroy domain' do
-    skip
-    assert_difference('Api::V1::Domain.count', -1) do
-      delete :destroy, id: @domain
-    end
+  # test 'should destroy domain' do
+  #   assert_difference('Api::V1::Domain.count', -1) do
+  #     delete :destroy, id: @domain
+  #   end
 
-    assert_redirected_to domains_path
-  end
+  #   assert_redirected_to domains_path
+  # end
 end
