b6531115bda793f1eb9ecd015e79e9cbadb46de7
Remo Mueller <remosm@gmail.com>
2016-06-13 11:33:26 -0400

    Make sure to run configure_permitted_parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 # Main web application controller for NSRR website. Keeps track of user's
 # location for friendly forwarding.
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
