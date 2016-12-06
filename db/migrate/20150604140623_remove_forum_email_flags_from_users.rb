e537d11bcd66070c9e90622458da4c4fb640b48a
Remo Mueller <remosm@gmail.com>
2016-12-06 12:07:34 -0500

    Fixed incorrect reverse migration

--- db/migrate/20150604140623_remove_forum_email_flags_from_users.rb
+++ db/migrate/20150604140623_remove_forum_email_flags_from_users.rb
@@ -1,6 +1,6 @@
 class RemoveForumEmailFlagsFromUsers < ActiveRecord::Migration[4.2]
   def change
-    remove_column :users, :auto_subscribe, null: false, default: false
-    remove_column :users, :email_me_when_mentioned, null: false, default: true
+    remove_column :users, :auto_subscribe, :boolean, null: false, default: false
+    remove_column :users, :email_me_when_mentioned, :boolean, null: false, default: true
   end
 end
