317a40168dd10c68a1594162eeeb67969c26f56c
Remo Mueller <remosm@gmail.com>
2017-01-26 15:16:21 -0500

    Version bump to 0.26.0.rc

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,7 +5,7 @@ module WwwSleepdataOrg
     MAJOR = 0
     MINOR = 26
     TINY = 0
-    BUILD = 'beta2' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = 'rc' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
