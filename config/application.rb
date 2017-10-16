31abec3e313fb90eec5a85170b7da50d520c2bf1
Remo Mueller <remosm@gmail.com>
2017-10-16 15:37:25 -0400

    Specify new gems.rb file for travis-ci

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in gems.rb, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
