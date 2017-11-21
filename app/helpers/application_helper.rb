a48beee3e8f020568e78546e993675ae8a3c53f4
Remo Mueller <remosm@gmail.com>
2017-11-21 12:46:53 -0500

    Updated forms for hosting requests, images, and topics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,20 +2,6 @@
 
 # Generic methods uses across the application views.
 module ApplicationHelper
-  def cancel
-    url = \
-      if URI.parse(request.referer.to_s).path.blank?
-        root_path
-      else
-        URI.parse(request.referer.to_s).path
-      end
-    link_to(
-      "Cancel",
-      url,
-      class: "btn btn-light"
-    )
-  end
-
   def simple_check(checked)
     content_tag(:i, "", class: "fa #{checked ? "fa-check-square-o" : "fa-square-o"}")
   end
