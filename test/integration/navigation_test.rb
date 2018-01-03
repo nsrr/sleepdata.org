9ab3f903e4f9e17debe43d1b83eadb5990af292b
Remo Mueller <remosm@gmail.com>
2018-01-03 15:13:58 -0500

    Updated navigation test

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
-SimpleCov.command_name 'test:integration'
+SimpleCov.command_name "test:integration"
 
 # Tests to assure that user navigation is working as intended.
 class NavigationTest < ActionDispatch::IntegrationTest
@@ -13,54 +13,54 @@ class NavigationTest < ActionDispatch::IntegrationTest
     @deleted = users(:deleted)
   end
 
-  test 'should get root path' do
-    get '/'
-    assert_equal '/', path
+  test "should get root path" do
+    get "/"
+    assert_equal "/", path
   end
 
-  test 'should get sign up page' do
+  test "should get sign up page" do
     get new_user_registration_path
     assert_equal new_user_registration_path, path
     assert_response :success
   end
 
-  test 'should register new account' do
+  test "should register new account" do
     post user_registration_path, params: {
       user: {
-        first_name: 'register', last_name: 'account',
-        email: 'register@account.com', password: 'registerpassword098765',
-        password_confirmation: 'registerpassword098765', emails_enabled: '1'
+        first_name: "register", last_name: "account",
+        email: "register@account.com", password: "registerpassword098765",
+        password_confirmation: "registerpassword098765", emails_enabled: "1"
       }
     }
-    assert_equal I18n.t('devise.registrations.signed_up'), flash[:notice]
+    assert_equal I18n.t("devise.registrations.signed_up"), flash[:notice]
     assert_redirected_to root_path
   end
 
-  test 'should not login deleted user' do
+  test "should not login deleted user" do
     get new_user_session_path
-    sign_in_as(@deleted, '123456')
+    sign_in_as(@deleted, "123456")
     assert_equal new_user_session_path, path
   end
 
-  test 'friendly url forwarding after login' do
+  test "friendly url forwarding after login" do
     get datasets_path
     get new_user_session_path
-    sign_in_as(@valid, '123456')
+    sign_in_as(@valid, "123456")
     assert_equal datasets_path, path
   end
 
-  test 'friendly url forwarding after logout' do
+  test "friendly url forwarding after logout" do
     get datasets_path
-    sign_in_as(@valid, '123456')
+    sign_in_as(@valid, "123456")
     get datasets_path
     get destroy_user_session_path
     assert_redirected_to datasets_path
   end
 
-  test 'blog rss should not be stored in friendly forwarding after login' do
-    get blog_path(format: 'atom')
+  test "blog rss should not be stored in friendly forwarding after login" do
+    get blog_path(format: "atom")
     get new_user_session_path
-    sign_in_as(@valid, '123456')
+    sign_in_as(@valid, "123456")
     assert_equal root_path, path
   end
 end
