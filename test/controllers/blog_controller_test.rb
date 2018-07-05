cfcc6a6b7799197fdcbe974a3e12979899a7285a
Remo Mueller <remosm@gmail.com>
2018-07-05 15:51:48 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/blog_controller_test.rb
+++ test/controllers/blog_controller_test.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Test to make sure blog is publicly accessible
-class BlogControllerTest < ActionController::TestCase
-  test 'should get blog' do
-    get :blog
+class BlogControllerTest < ActionDispatch::IntegrationTest
+  test "should get blog" do
+    get blog_url
     assert_response :success
   end
 
-  test 'should get blog atom feed' do
-    get :blog, params: { format: 'atom' }
+  test "should get blog atom feed" do
+    get blog_url(format: "atom")
     assert_response :success
   end
 
-  test 'should show published blog' do
-    get :show, params: { slug: broadcasts(:published).slug }
+  test "should show published blog" do
+    get blog_slug_url(broadcasts(:published).slug)
     assert_response :success
   end
 
-  test 'should not show draft blog' do
-    get :show, params: { slug: broadcasts(:draft).slug }
-    assert_redirected_to blog_path
+  test "should not show draft blog" do
+    get blog_slug_url(broadcasts(:draft).slug)
+    assert_redirected_to blog_url
   end
 end
