9bfa2ca89cc09147e4992f788c7071392a7ce657
Remo Mueller <remosm@gmail.com>
2018-07-06 10:51:15 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/variables_controller_test.rb
+++ test/controllers/variables_controller_test.rb
@@ -3,89 +3,67 @@
 require "test_helper"
 
 # Tests to assure that variables can be viewed.
-class VariablesControllerTest < ActionController::TestCase
+class VariablesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @dataset = datasets(:released)
     @variable = datasets(:released).variables.first
   end
 
   test "should get index for public dataset" do
-    get :index, params: { dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variables)
+    get dataset_variables_url(@dataset)
     assert_response :success
   end
 
   test "should show public variable to public user" do
-    get :show, params: { id: @variable, dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variable)
+    get dataset_variable_url(@dataset, @variable)
     assert_response :success
   end
 
   test "should show public variable to logged in user" do
     login(users(:valid))
-    get :show, params: { id: @variable, dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variable)
+    get dataset_variable_url(@dataset, @variable)
     assert_response :success
   end
 
   test "should show unreleased variable to logged in user" do
     login(users(:admin))
-    get :show, params: { id: variables(:private), dataset_id: datasets(:unreleased) }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variable)
+    get dataset_variable_url(datasets(:unreleased), variables(:private))
     assert_response :success
   end
 
   test "should redirect to public dataset for unknown variable" do
-    get :show, params: { id: "notavariable", dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_nil assigns(:variable)
-    assert_redirected_to dataset_variables_path(assigns(:dataset))
+    get dataset_variable_url(@dataset, "notavariable")
+    assert_redirected_to dataset_variables_url(assigns(:dataset))
   end
 
   test "should not show unreleased variable to public user" do
-    get :show, params: { id: variables(:private), dataset_id: datasets(:unreleased) }
-    assert_nil assigns(:dataset)
-    assert_nil assigns(:variable)
-    assert_redirected_to datasets_path
+    get dataset_variable_url(datasets(:unreleased), variables(:private))
+    assert_redirected_to datasets_url
   end
 
   test "should not show unreleased variable to logged in user" do
     login(users(:valid))
-    get :show, params: { id: variables(:private), dataset_id: datasets(:unreleased) }
-    assert_nil assigns(:dataset)
-    assert_nil assigns(:variable)
-    assert_redirected_to datasets_path
+    get dataset_variable_url(datasets(:unreleased), variables(:private))
+    assert_redirected_to datasets_url
   end
 
   test "should show public variable graphs to public user" do
-    get :graphs, params: { id: @variable, dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variable)
+    get graphs_dataset_variable_url(@dataset, @variable)
     assert_response :success
   end
 
   test "should show public variable history to public user" do
-    get :history, params: { id: @variable, dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variable)
+    get history_dataset_variable_url(@dataset, @variable)
     assert_response :success
   end
 
   test "should show public variable known_issues to public user" do
-    get :known_issues, params: { id: @variable, dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variable)
+    get known_issues_dataset_variable_url(@dataset, @variable)
     assert_response :success
   end
 
   test "should show public variable related to public user" do
-    get :related, params: { id: @variable, dataset_id: @dataset }
-    assert_not_nil assigns(:dataset)
-    assert_not_nil assigns(:variable)
+    get related_dataset_variable_url(@dataset, @variable)
     assert_response :success
   end
 end
