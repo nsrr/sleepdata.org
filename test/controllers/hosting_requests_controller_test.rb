f6f37541e700a5e577f4625e830c8d092c8fe7f8
Remo Mueller <remosm@gmail.com>
2018-07-06 10:19:36 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/hosting_requests_controller_test.rb
+++ test/controllers/hosting_requests_controller_test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 # Tests to assure admins can review hosting requests.
-class HostingRequestsControllerTest < ActionController::TestCase
+class HostingRequestsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @hosting_request = hosting_requests(:one)
     login(users(:admin))
@@ -13,47 +13,47 @@ class HostingRequestsControllerTest < ActionController::TestCase
     {
       institution_name: @hosting_request.institution_name,
       description: @hosting_request.description,
-      reviewed: '1'
+      reviewed: "1"
     }
   end
 
-  test 'should get index' do
-    get :index
+  test "should get index" do
+    get hosting_requests_url
     assert_not_nil assigns(:hosting_requests)
     assert_response :success
   end
 
-  # test 'should get new' do
-  #   get :new
+  # test "should get new" do
+  #   get new_hosting_request_url
   #   assert_response :success
   # end
 
-  # test 'should create hosting_request' do
-  #   assert_difference('HostingRequest.count') do
-  #     post :create, params: { hosting_request: hosting_request_params }
+  # test "should create hosting_request" do
+  #   assert_difference("HostingRequest.count") do
+  #     post hosting_requests_url, params: { hosting_request: hosting_request_params }
   #   end
-  #   assert_redirected_to hosting_request_path(assigns(:hosting_request))
+  #   assert_redirected_to hosting_request_url(assigns(:hosting_request))
   # end
 
-  test 'should show hosting_request' do
-    get :show, params: { id: @hosting_request }
+  test "should show hosting_request" do
+    get hosting_request_url(@hosting_request)
     assert_response :success
   end
 
-  test 'should get edit' do
-    get :edit, params: { id: @hosting_request }
+  test "should get edit" do
+    get edit_hosting_request_url(@hosting_request)
     assert_response :success
   end
 
-  test 'should update hosting_request' do
-    patch :update, params: { id: @hosting_request, hosting_request: hosting_request_params }
-    assert_redirected_to hosting_request_path(assigns(:hosting_request))
+  test "should update hosting_request" do
+    patch hosting_request_url(@hosting_request), params: { hosting_request: hosting_request_params }
+    assert_redirected_to hosting_request_url(assigns(:hosting_request))
   end
 
-  test 'should destroy hosting_request' do
-    assert_difference('HostingRequest.current.count', -1) do
-      delete :destroy, params: { id: @hosting_request }
+  test "should destroy hosting_request" do
+    assert_difference("HostingRequest.current.count", -1) do
+      delete hosting_request_url(@hosting_request)
     end
-    assert_redirected_to hosting_requests_path
+    assert_redirected_to hosting_requests_url
   end
 end
