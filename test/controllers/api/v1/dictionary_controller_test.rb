2f73c077d6d34905920f79f08d041434e162495a
Remo Mueller <remosm@gmail.com>
2018-07-06 11:08:22 -0400

    Updated test to ActionDispatch::IntegrationTest

--- test/controllers/api/v1/dictionary_controller_test.rb
+++ test/controllers/api/v1/dictionary_controller_test.rb
@@ -2,9 +2,10 @@
 
 require "test_helper"
 
-class Api::V1::DictionaryControllerTest < ActionController::TestCase
+# Test to check that dataset files can be uploaded.
+class Api::V1::DictionaryControllerTest < ActionDispatch::IntegrationTest
   test "should get success if file is uploaded" do
-    post :upload_file, params: {
+    post api_v1_dictionary_upload_file_url, params: {
       dataset: datasets(:released).to_param,
       auth_token: users(:editor).id_and_auth_token,
       file: fixture_file_upload("../../test/support/images/rails.png"),
@@ -16,7 +17,7 @@ class Api::V1::DictionaryControllerTest < ActionController::TestCase
   end
 
   test "should get failed if file does not exist after upload" do
-    post :upload_file, params: {
+    post api_v1_dictionary_upload_file_url, params: {
       dataset: datasets(:released).to_param,
       auth_token: users(:editor).id_and_auth_token,
       file: ""
