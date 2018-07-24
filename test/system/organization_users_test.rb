592eb6fe657bf9cee7c405b6cb3372c4543f06ca
Remo Mueller <remosm@gmail.com>
2018-07-24 10:39:29 -0400

    Update organization team system tests

--- test/system/organization_users_test.rb
+++ test/system/organization_users_test.rb
@@ -1,55 +1,80 @@
+# frozen_string_literal: true
+
 require "application_system_test_case"
 
+# System tests for adding and removing organization members.
 class OrganizationUsersTest < ApplicationSystemTestCase
   setup do
+    @editor = users(:editor)
+    @organization = organizations(:one)
     @organization_user = organization_users(:one)
   end
 
   test "visiting the index" do
-    visit organization_users_url
-    assert_selector "h1", text: "Organization Users"
+    visit organization_url(@organization)
+    screenshot("organization-visit-team-index")
+    click_on "Members"
+    screenshot("organization-visit-team-index")
+    click_on "Sign in"
+    fill_in "user[email]", with: @editor.email
+    fill_in "user[password]", with: "password"
+    screenshot("organization-visit-team-index")
+    click_form_submit
+    assert_selector "h1", text: "Organization One"
+    screenshot("organization-visit-team-index")
   end
 
-  test "creating a Organization user" do
-    visit organization_users_url
-    click_on "New Organization User"
+  test "inviting an organization member" do
+    visit organization_organization_users_url(@organization)
+    click_on "Sign in"
+    fill_in "user[email]", with: @editor.email
+    fill_in "user[password]", with: "password"
+    click_form_submit
 
-    fill_in "Creator", with: @organization_user.creator_id
-    fill_in "Editor", with: @organization_user.editor
-    fill_in "Invite Email", with: @organization_user.invite_email
-    fill_in "Invite Token", with: @organization_user.invite_token
-    fill_in "Organization", with: @organization_user.organization_id
-    fill_in "Review Role", with: @organization_user.review_role
-    fill_in "User", with: @organization_user.user_id
-    click_on "Create Organization user"
+    screenshot("organization-invite-member")
+    click_on "Invite member"
+    fill_in "organization_user[invite_email]", with: "viewer_invite@example.com"
+    # fill_in "organization_user[review_role]", with: @organization_user.review_role
+    # fill_in "organization_user[editor]", with: @organization_user.editor
+    screenshot("organization-invite-member")
+    click_on "Send invitation"
 
-    assert_text "Organization user was successfully created"
-    click_on "Back"
+    assert_text "Organization member was successfully invited."
+    screenshot("organization-invite-member")
   end
 
-  test "updating a Organization user" do
-    visit organization_users_url
-    click_on "Edit", match: :first
+  test "updating an organization member" do
+    visit organization_organization_users_url(@organization)
+    click_on "Sign in"
+    fill_in "user[email]", with: @editor.email
+    fill_in "user[password]", with: "password"
+    click_form_submit
 
-    fill_in "Creator", with: @organization_user.creator_id
-    fill_in "Editor", with: @organization_user.editor
-    fill_in "Invite Email", with: @organization_user.invite_email
-    fill_in "Invite Token", with: @organization_user.invite_token
-    fill_in "Organization", with: @organization_user.organization_id
-    fill_in "Review Role", with: @organization_user.review_role
-    fill_in "User", with: @organization_user.user_id
-    click_on "Update Organization user"
+    screenshot("organization-update-member")
 
-    assert_text "Organization user was successfully updated"
-    click_on "Back"
+    click_on "Actions", match: :first
+    click_on "Edit"
+    screenshot("organization-update-member")
+    select "Principal Reviewer", from: "organization_user[review_role]"
+    click_on "Update member"
+    assert_text "Organization member was successfully updated."
+    screenshot("organization-update-member")
   end
 
-  test "destroying a Organization user" do
-    visit organization_users_url
+  test "destroying an organization member" do
+    visit organization_organization_users_url(@organization)
+    click_on "Sign in"
+    fill_in "user[email]", with: @editor.email
+    fill_in "user[password]", with: "password"
+    click_form_submit
+    screenshot("organization-remove-member")
+    # click_on "Actions", match: :second # The following is emulating this.
+    page.all(".dropdown-toggle")[2].click
+    screenshot("organization-remove-member")
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on "Delete"
     end
-
-    assert_text "Organization user was successfully destroyed"
+    assert_text "Organization member was successfully deleted."
+    screenshot("organization-remove-member")
   end
 end
