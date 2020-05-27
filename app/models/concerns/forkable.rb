af82b888577c83816fb08966e847d681391d1948
Remo Mueller <remosm@gmail.com>
2020-05-27 18:45:00 -0400

    Fix Ruby 3.0 deprecation warning: Using the last argument as keyword parameters is deprecated

--- app/models/concerns/forkable.rb
+++ app/models/concerns/forkable.rb
@@ -5,19 +5,46 @@ module Forkable
   extend ActiveSupport::Concern
 
   def fork_process(method, *args)
-    if Rails.env.test?
-      send method, *args
+    if args.last.is_a? Hash
+      hash = args.pop
+      fork_process_with_hash(method, args, hash)
     else
-      create_fork method, *args
+      fork_process_with_array(method, args)
     end
   end
 
   private
 
-  def create_fork(method, *args)
+  def fork_process_with_array(method, args)
+    if Rails.env.test?
+      send method, *args
+    else
+      create_fork_with_array method, *args
+    end
+  end
+
+  def fork_process_with_hash(method, args, hash)
+    if Rails.env.test?
+      send method, *args, **hash
+    else
+      create_fork_with_hash method, *args, **hash
+    end
+  end
+
+  def create_fork_with_array(method, args)
     pid = Process.fork
     if pid.nil?
       send method, *args
+      Kernel.exit!
+    else
+      Process.detach pid
+    end
+  end
+
+  def create_fork_with_hash(method, args, hash)
+    pid = Process.fork
+    if pid.nil?
+      send method, *args, **hash
       Kernel.exit!
     else
       Process.detach pid
