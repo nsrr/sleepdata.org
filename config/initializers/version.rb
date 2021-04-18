125d5188f80b44852c97f37c4000474aa1729736
Remo Mueller <remosm@gmail.com>
2021-04-18 13:30:29 -0400

    Version bump to 52.0.0.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,10 +2,10 @@
 
 module SleepData
   module VERSION #:nodoc:
-    MAJOR = 51
-    MINOR = 2
+    MAJOR = 52
+    MINOR = 0
     TINY = 0
-    BUILD = nil # "pre", "beta1", "beta2", "rc", "rc2", nil
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
   end
