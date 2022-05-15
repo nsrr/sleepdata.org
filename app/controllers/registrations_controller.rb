1457672079d8a22aa41fc97189fa85db477d781e
Remo Mueller <remosm@gmail.com>
2022-05-15 11:26:42 -0400

    Move text captcha back to sign up form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,9 +3,4 @@
 # Override for devise registrations controller.
 class RegistrationsController < Devise::RegistrationsController
   layout "full_page"
-
-  def new
-    super
-    resource.textcaptcha
-  end
 end
